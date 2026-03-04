b1cc25a384e074f8aadd577bc35b32cda8651b14
ss-ngm-n <sungmin10057@naver.com>
2026-03-04 22:44:28 +0900

    chore: CORS

---
+++ src/main/java/com/root/root/config/CorsConfig.java
@@ -0,0 +1,13 @@
+package com.root.root.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// Next.js 주소의 접근 허용
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://rootdev.netlify.app").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*").allowCredentials(true);
+    }
+}
