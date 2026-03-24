9b9272165ba446c2f7e553271175e4c395074fbd
hs-2191214-parkjunseo <02justine@naver.com>
2026-03-24 19:53:49 +0900

    feat : 자격증 정보 조회

--- src/main/java/com/root/root/controller/ExamController.java
+++ src/main/java/com/root/root/controller/ExamController.java
@@ -1,9 +1,9 @@
 package com.root.root.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,8 +21,14 @@ public class ExamController {
     }
 
     //  DB 데이터 API
-    @GetMapping("/all") 
+    @GetMapping("/all")
     public List<ExamData> getAllExams() {
         return examDataRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ExamData getExamById(@PathVariable Long id) {
+        return examDataRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("해당 자격증을 찾을 수 없습니다."));
     }
 }
