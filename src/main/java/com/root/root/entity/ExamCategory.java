9c0e1f1d6d7f479a7a8d6de7c134a7088f0f3bcb
hs-2191214-parkjunseo <02justine@naver.com>
2026-05-11 00:08:15 +0900

    fix : 무한루프 방지

--- src/main/java/com/root/root/entity/ExamCategory.java
+++ src/main/java/com/root/root/entity/ExamCategory.java
@@ -5,8 +5,21 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter @Setter
@@ -33,6 +46,7 @@ public class ExamCategory {
 
     // ExamData와의 연결 (양방향)
     @OneToMany(mappedBy = "examCategory")
+    @JsonIgnore
     @Builder.Default
     private List<ExamData> exams = new ArrayList<>();
 }
